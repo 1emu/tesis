67888d1c67df48f034e2f95d068a4cd78dad40d2
jparnaudo <jparnaudo@10pines.com>
2017-09-06 01:24:14 -0300

    - Agrego los niveles para principiantes del zoologics

--- app/zoologic/ZoologicLevels.js
+++ app/zoologic/ZoologicLevels.js
@@ -1,5 +1,5 @@
 export const DIFFICULTY = {BEGINNER: 'BEGINNER', INTERMEDIATE: 'INTERMEDIATE', ADVANCED: 'ADVANCED', EXPERT: 'EXPERT'};
-export const MAX_LEVEL_NUMBER = 40;
+export const MAX_LEVEL_NUMBER = 10;
 
 export default class ZoologicLevels {
 
@@ -35,7 +35,135 @@ export default class ZoologicLevels {
       '----------',
       pieces: ['D', 'D', 'O'],
       difficulty: DIFFICULTY.BEGINNER
-    }
+    },
+    {
+      levelNumber: 3,
+      squares:
+      '----------' +
+      '----------' +
+      '-----HH---' +
+      '-----HH---' +
+      '-----XX---' +
+      '-----XX---' +
+      '---XXXX---' +
+      '---XXXX---' +
+      '----------' +
+      '----------',
+      pieces: ['M', 'O', 'C'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 4,
+      squares:
+      '----------' +
+      '----------' +
+      '--HHXX----' +
+      '--HHXX----' +
+      '--XXXXXX--' +
+      '--XXXXXX--' +
+      '----------' +
+      '----------' +
+      '----------' +
+      '----------',
+      pieces: ['C', 'C', 'O', 'M'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 5,
+      squares:
+      '----------' +
+      '----------' +
+      '----XXXX--' +
+      '----XXXX--' +
+      '--XXXXOO--' +
+      '--XXXXOO--' +
+      '----------' +
+      '----------' +
+      '----------' +
+      '----------',
+      pieces: ['O', 'C', 'D', 'M'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 6,
+      squares:
+      '----------' +
+      '----------' +
+      '----XX----' +
+      '----XX----' +
+      '----XX----' +
+      '----XX----' +
+      '----XX----' +
+      '----XX----' +
+      '----XX----' +
+      '----XX----',
+      pieces: ['C', 'O', 'M', 'D'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 7,
+      squares:
+      '----------' +
+      '----------' +
+      '--XXXXXX--' +
+      '--XXXXXX--' +
+      '----XX----' +
+      '----XX----' +
+      '----------' +
+      '----------' +
+      '----------' +
+      '----------',
+      pieces: ['H', 'H', 'D', 'M'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 8,
+      squares:
+      '----------' +
+      '----------' +
+      '--DDXXFF--' +
+      '--DDXXFF--' +
+      '--XXXXXX--' +
+      '--XXXXXX--' +
+      '----XX----' +
+      '----XX----' +
+      '----------' +
+      '----------',
+      pieces: ['F', 'M', 'O', 'O', 'C'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 9,
+      squares:
+      '----------' +
+      '----------' +
+      '---XXHH---' +
+      '---XXHH---' +
+      '--XXXXXX--' +
+      '--XXXXXX--' +
+      '----------' +
+      '----------' +
+      '----------' +
+      '----------',
+      pieces: ['D', 'D', 'M', 'H'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
+    {
+      levelNumber: 10,
+      squares:
+      '----------' +
+      '----------' +
+      '--XXXX----' +
+      '--XXXX----' +
+      '--XXXX----' +
+      '--XXXX----' +
+      '----XX----' +
+      '----XX----' +
+      '----------' +
+      '----------',
+      pieces: ['O', 'C', 'C', 'H', 'D'],
+      difficulty: DIFFICULTY.BEGINNER
+    },
   ];
 
   static getLevelConfig(levelNumber) {
