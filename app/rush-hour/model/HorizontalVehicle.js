ba1a8cbc991294e45756d012dff6b7ba4411f600
jparnaudo <jparnaudo@10pines.com>
2017-10-16 13:20:46 -0300

    - Movemos comportamiento para limitar el movimiento de los vehiculos a las subclases de vehiculos, y eliminamos los cuadraditos horizontales y verticales

--- app/rush-hour/model/HorizontalVehicle.js
+++ app/rush-hour/model/HorizontalVehicle.js
@@ -1,9 +1,13 @@
-import CuadraditoHorizontal from "./CuadraditoHorizontal";
 import CarImage from "./CarImages";
+import Cuadradito from './Cuadradito';
 
-export default class HorizontalVehicle extends CuadraditoHorizontal {
+export default class HorizontalVehicle extends Cuadradito {
   constructor(id, x = 0, y = 0, tablero, color, orientation = 'left', vehicleType) {
     let image = CarImage.for(vehicleType.typeName, color, orientation);
     super(id, x, y, vehicleType.lengthInTiles, 1, tablero, image);
+  }
+
+  setPosition(x, y) {
+    this.x = x;
   }
 }
--- app/rush-hour/model/VerticalVehicle.js
+++ app/rush-hour/model/VerticalVehicle.js
@@ -1,9 +1,13 @@
-import CuadraditoVertical from "./CuadraditoVertical";
 import CarImage from "./CarImages";
+import Cuadradito from './Cuadradito';
 
-export default class VerticalVehicle extends CuadraditoVertical {
+export default class VerticalVehicle extends Cuadradito {
   constructor(id, x = 0, y = 0, tablero, color, orientation = 'up', vehicleType) {
     let image = CarImage.for(vehicleType.typeName, color, orientation);
     super(id, x, y, 1, vehicleType.lengthInTiles, tablero, image);
+  }
+
+  setPosition(x, y) {
+    this.y = y;
   }
 }
